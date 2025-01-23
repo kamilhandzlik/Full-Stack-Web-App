41e152f9522c57c9f68c29f3f29fd29da2293dce
kamol0209 <kamol0209@gmail.com>
2025-01-23 09:51:04 +0100

    Added model and serializer

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
-from .views import main
+from django.urls import path
+from . import views
 
 
-urlpatterns = [path("", main, name="main")]
+urlpatterns = [path("home", views.RoomView.as_view(), name="home")]
