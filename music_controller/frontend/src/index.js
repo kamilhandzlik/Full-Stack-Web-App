c549d9e57b7fa39b913affd3668b236cd11abf28
kamol0209 <kamol0209@gmail.com>
2025-01-27 10:04:58 +0100

    Added react routes

--- music_controller/frontend/src/index.js
+++ music_controller/frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-const appDiv = document.getElementById("app"); 
+const appDiv = document.getElementById("app");
 if (appDiv) {
   const root = createRoot(appDiv);
   root.render(<App />);
